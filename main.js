84ef9da736985e4b2581383f7678eef34a29594d
sai.arja@langoor.com <sai.arja@langoor.com>
2023-10-18 21:34:43 +0530

    new pages created about us and contact us

--- main.js
+++ main.js
@@ -15,6 +15,29 @@ $(document).ready(function () {
             }
         }
     })
+    $(".close-button").on('click', function () {
+        console.log('it is working')
+        $('.top-advt').remove()
+    });
+
+    $('.form-bt').on('click', function () {
+        var value = $('#name').val();
+        // alert(value);
+        console.log(value)
+        var value = $('#phone').val();
+        // alert(value);
+        console.log(value)
+        var value = $('#email').val();
+        // alert(value);
+        console.log(value)
+        var value = $('#city').val();
+        // alert(value);
+        console.log(value)
+        var value = $('#email').val();
+        // alert(value);
+        console.log(value)
+    });
+
 });
 
 
